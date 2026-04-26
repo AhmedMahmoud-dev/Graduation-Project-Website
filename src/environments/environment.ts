50ebf47495e1485929897ec7e321087c4dac8eb7
Ahmed Mahmoud <116674098+AhmedMahmoud-dev@users.noreply.github.com>
2026-04-26 23:55:09 +0300

    add audio api proxy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,10 @@
 export const environment = {
   production: false,
   apiUrl: "https://emotion-detection.runasp.net",
-  textApiUrl: "http://13.63.140.123:8000",
-  audioApiUrl: "http://13.51.255.179:8001",
+  // textApiUrl: "http://13.63.140.123:8000",
+  // audioApiUrl: "http://13.51.255.179:8001",
+  textApiUrl: "https://graduation-project-website-eight.vercel.app/api",
+  audioApiUrl: "https://graduation-project-website-eight.vercel.app/api",
   tokenKey: "emotra_token",
   userKey: "emotra_user",
 };
