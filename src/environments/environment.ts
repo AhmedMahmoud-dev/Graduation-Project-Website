1bd870a2787fa1b47c1d1dcd1627a691f4667c2c
Ahmed Mahmoud <116674098+AhmedMahmoud-dev@users.noreply.github.com>
2026-05-25 17:33:52 +0300

    config: update text and audio API URLs to emotra.vercel.app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,10 @@ export const environment = {
   apiUrl: "https://emotion-detection.runasp.net",
   // textApiUrl: "http://13.63.140.123:8000",
   // audioApiUrl: "http://13.51.255.179:8001",//
-  textApiUrl: "https://graduation-project-website-eight.vercel.app/text",
-  audioApiUrl: "https://graduation-project-website-eight.vercel.app/audio",
+  // textApiUrl: "https://graduation-project-website-eight.vercel.app/text",
+  // audioApiUrl: "https://graduation-project-website-eight.vercel.app/audio",
+  textApiUrl: "https://emotra.vercel.app/text",
+  audioApiUrl: "https://emotra.vercel.app/audio",
   imageApiUrl: "http://127.0.0.1:8004",
   videoApiUrl: "http://127.0.0.1:8004",
   tokenKey: "emotra_token",
